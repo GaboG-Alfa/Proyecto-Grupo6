707ae564a1c54facb47c1e39afacee2c22dee07c
Gabriel Guzmán Alfaro <80297101+GaboG-Alfa@users.noreply.github.com>
2024-07-05 17:13:33 -0600

    Add files via upload
    Sea agregó carpeta BL

---
+++ BL/AdminBL.cs
@@ -0,0 +1,80 @@
+﻿using DA;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class AdminBL
+    {
+        private readonly AdminDA _adminDA;
+
+        public AdminBL()
+        {
+            _adminDA = new AdminDA();
+        }
+
+        public bool LogIn(Usuarios admin)
+        {
+            bool adminExists = _adminDA.LogIn(admin);
+            return adminExists;
+        }
+        public Usuarios ObtenerUsuarioPorEmail(string email)
+        {
+            return _adminDA.ObtenerUsuarioPorEmail(email);
+        }
+
+        public List<Productos> ObtenerProductos()
+        {
+            return _adminDA.ObtenerProductos();
+        }
+
+        public Productos ObtenerProductoPorId(int id)
+        {
+            return _adminDA.ObtenerProductoPorId(id);
+        }
+
+        public void AgregarProducto(Productos producto)
+        {
+            _adminDA.AgregarProducto(producto);
+        }
+
+        public void EditarProducto(Productos producto)
+        {
+            _adminDA.EditarProducto(producto);
+        }
+
+        public void EliminarProducto(int id)
+        {
+            _adminDA.EliminarProducto(id);
+        }
+
+        public List<Categorias> ObtenerCategorias()
+        {
+            return _adminDA.ObtenerCategorias();
+        }
+
+        public Categorias ObtenerCategoriaPorId(int id)
+        {
+            return _adminDA.ObtenerCategoriaPorId(id);
+        }
+
+        public void AgregarCategoria(Categorias categoria)
+        {
+            _adminDA.AgregarCategoria(categoria);
+        }
+
+        public void EditarCategoria(Categorias categoria)
+        {
+            _adminDA.EditarCategoria(categoria);
+        }
+
+        public void EliminarCategoria(int id)
+        {
+            _adminDA.EliminarCategoria(id);
+        }
+    }
+}
---
+++ BL/ClienteBL.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using DA;
+using Models;
+
+namespace BL
+{
+    public class ClienteBL
+    {
+        private readonly ClienteDA _clientDA;
+
+        public ClienteBL()
+        {
+            _clientDA = new ClienteDA();
+        }
+
+        public bool LogIn(Usuarios client)
+        {
+            return _clientDA.LogIn(client);
+        }
+
+        public IEnumerable<Productos> ObtenerTodosLosProductos()
+        {
+            return _clientDA.ObtenerTodosLosProductos();
+        }
+
+        public Productos ObtenerProductoPorId(int id)
+        {
+            return _clientDA.ObtenerProductoPorId(id);
+        }
+
+        public IEnumerable<Productos> BuscarProductos(string nombre, string categoria, string feature1, string feature2)
+        {
+            return _clientDA.BuscarProductos(nombre, categoria, feature1, feature2);
+        }
+
+        public void AñadirProductoAlCarrito(int userId, int productoId, int cantidad)
+        {
+            _clientDA.AñadirProductoAlCarrito(userId, productoId, cantidad);
+        }
+
+        public void EliminarProductoDelCarrito(int userId, int productoId)
+        {
+            _clientDA.EliminarProductoDelCarrito(userId, productoId);
+        }
+
+        public void ActualizarCantidadProductoEnCarrito(int userId, int productoId, int cantidad)
+        {
+            _clientDA.ActualizarCantidadProductoEnCarrito(userId, productoId, cantidad);
+        }
+
+        public IEnumerable<CarritoCompras> ObtenerCarritoDeCompras(int userId)
+        {
+            return _clientDA.ObtenerCarritoDeCompras(userId);
+        }
+
+        public void CrearOrden(Ordenes orden)
+        {
+            _clientDA.CrearOrden(orden);
+        }
+
+        public Ordenes ObtenerOrdenPorId(int ordenId)
+        {
+            return _clientDA.ObtenerOrdenPorId(ordenId);
+        }
+
+        public IEnumerable<Ordenes> ObtenerOrdenesPorUsuario(int userId)
+        {
+            return _clientDA.ObtenerOrdenesPorUsuario(userId);
+        }
+
+        public Usuarios ObtenerUsuarioPorEmail(string email)
+        {
+            return _clientDA.ObtenerUsuarioPorEmail(email);
+        }
+
+        public Usuarios ObtenerUsuarioPorId(int usuarioId)
+        {
+            return _clientDA.ObtenerUsuarioPorId(usuarioId);
+        }
+    }
+}
